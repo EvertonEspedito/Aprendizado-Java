dbcac61f7b1d207bc171f8f75e21848d52186aea
Everton Espedito <evertondev505@gmail.com>
2025-12-18 12:31:42 -0300

    Projeto Completo de Gerador de Senha
    Projeto Utilizando JavaFX

--- geradorSenha/FrontGeradorSenhas.java
+++ geradorSenha/FrontGeradorSenhas.java
@@ -6,7 +6,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
-
+import javafx.geometry.Insets;
 
 // javac --module-path $PATH_TO_FX --add-modules javafx.controls FrontGeradorSenhas.java
 
@@ -20,27 +20,38 @@ public class FrontGeradorSenhas extends Application {
     	palco.setTitle("Gerador de Senhas Javafx");
 
     	Label labelTamanhoSenha = new Label("Tamanho da Senha");
+
     	TextField campoTamanhoSenha = new TextField();
     	campoTamanhoSenha.setText("8"); //Sugestão de Tamanho
-
-    	Button bGerarSenha = new Button("Gerar Senha");
-    	bGerarSenha.setOnAction( e ->{
-    		// Quando apertar o botão
-    	})
 
     	Label senhaGerada = new Label("Senha Gerada:");
 
     	TextField campoSenhaGerada = new TextField();
     	campoSenhaGerada.setEditable(false);
-    	//3:56
-    	
+    	campoSenhaGerada.setStyle("-fx-background-color: black; -fx-text-fill: Cyan;");
+
+    	Button bGerarSenha = new Button("Gerar Senha");
+    	bGerarSenha.setOnAction( e ->{
+    		// Quando apertar o botão
+    		int tamanhoSenha = Integer.parseInt(campoTamanhoSenha.getText());// Pegar tamanho da senha
+    		String senha = GeradorDeSenhas.gerarSenhas(tamanhoSenha);
+
+    		campoSenhaGerada.setText(senha);
+    	});
+    	bGerarSenha.setStyle(
+    		"-fx-background-color: green;" +
+    		"-fx-text-fill: white;" +
+    		"-fx-background-radius: 5;"
+		);
+
     	//Layout Vertical
     	VBox layout = new VBox(labelTamanhoSenha, campoTamanhoSenha, bGerarSenha
     		, senhaGerada, campoSenhaGerada);
     	layout.setAlignment(Pos.CENTER);
-      	layout.setStyle("-fx-background-color: gray;");
+      	layout.setSpacing(10); //Espaço entre os componentes!
+      	layout.setPadding(new Insets(10)); //Espaço entre as bordas!
     	//cena e palco
-      	Scene cena = new Scene(layout, 400, 200);
+      	Scene cena = new Scene(layout, 300, 200);
 
       	palco.setTitle("Gerador de Senha");
       	palco.setScene(cena);
