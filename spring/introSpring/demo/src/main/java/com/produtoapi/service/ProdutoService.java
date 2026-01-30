1bb5cb80c624eed43fa7664ee8cccd9fb1060b3a
Everton Espedito <evertondev505@gmail.com>
2026-01-30 12:00:46 -0300

    spring

--- spring/introSpring/demo/src/main/java/com/produtoapi/service/ProdutoService.java
+++ spring/introSpring/demo/src/main/java/com/produtoapi/service/ProdutoService.java
@@ -35,4 +35,69 @@ public class ProdutoService {
     public List<Produto> salvarLista(List<Produto> produtos) {
         return produtoRepository.saveAll(produtos);
     }
+    // ==============================================================================
+    // =================== BUSCAS DETALHADAS POR NOME DE PRODUTO ====================
+    // ==============================================================================
+    // Métodos de busca por nome
+    public List<Produto> findByNome(String nome) {
+        return produtoRepository.findByNome(nome);
+    }
+    public List<Produto> findByNomeContaining(String nome) {
+        return produtoRepository.findByNomeContaining(nome);
+    }
+    public List<Produto> findByNomeAndStatus(String nome, String status) {
+        return produtoRepository.findByNomeAndStatus(nome, status);
+    }
+    public List<Produto> findByNomeStartingWith(String prefix) {
+        return produtoRepository.findByNomeStartingWith(prefix);
+    }
+    public List<Produto> findByNomeEndingWith(String suffix) {
+        return produtoRepository.findByNomeEndingWith(suffix);
+    }
+
+    // ==============================================================================
+    // =================== BUSCAS DETALHADAS POR PREÇO ==============================
+    // ==============================================================================
+    public List<Produto> findByPreco(Double preco) {
+        return produtoRepository.findByPreco(preco);
+    }
+    public List<Produto> findByPrecoGreaterThan(Double preco) {
+        return produtoRepository.findByPrecoGreaterThan(preco);
+    }
+    public List<Produto> findByPrecoLessThan(Double preco) {
+        return produtoRepository.findByPrecoLessThan(preco);
+    }
+    public Double findTotalPreco() {
+        return produtoRepository.findTotalPreco();
+    }
+
+    // ==============================================================================
+    // ================ BUSCAS DETALHADAS POR QUANTIDADE DE PRODUTO =================
+    // ==============================================================================
+    public List<Produto> findByQuantidade(Integer quantidade) {
+        return produtoRepository.findByQuantidade(quantidade);
+    }
+    public List<Produto> findByQuantidadeLessThan(Integer quantidade) {
+        return produtoRepository.findByQuantidadeLessThan(quantidade);
+    }
+    public List<Produto> findByQuantidadeGreaterThan(Integer quantidade) {
+        return produtoRepository.findByQuantidadeGreaterThan(quantidade);
+    }
+
+    //==============================================================================
+    // ============= BUSCAS DETALHADAS POR STATUS E PREÇO DE PRODUTO================
+    //=============================================================================
+    public List<Produto> findByStatus(String status) {
+        return produtoRepository.findByStatus(status);
+    }
+    public List<Produto> findByStatusIsNull() {
+        return produtoRepository.findByStatusIsNull();
+    }
+    public List<Produto> findByPrecoAndStatus(Double preco, String status) {
+        return produtoRepository.findByPrecoAndStatus(preco, status);
+    }
+    // Método para trazer o número total de produtos
+    public Long count() {
+        return produtoRepository.count();
+    }
 }
